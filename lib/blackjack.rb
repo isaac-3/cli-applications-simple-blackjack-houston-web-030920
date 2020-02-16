1446ace1168fdafb2a029c1450fedeadb937ee9c
isaac c <isaacgc0596@gmail.com>
2020-02-16 23:56:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,14 +57,14 @@ end
 def runner
   welcome
   c_total = initial_round
-  # while c_total > 21 do
-  #   c_total = hit?(c_total)
-  #   display_card_total
-  # end
-  until c_total > 21 do
+  while c_total > 21 do
     c_total = hit?(c_total)
-    display_card_total(c_total)
+    display_card_total
   end
+  # until c_total > 21 do
+  #   c_total = hit?(c_total)
+  #   display_card_total(c_total)
+  # end
   # loop do
   #   c_total = hit?(c_total)
   #   display_card_total(c_total)
